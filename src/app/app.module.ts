a78973021e3beb43a7c85f8e1ac9dec29a6c81c8
prabukdatasirpi <prabuk@datasirpi.com>
2022-12-05 18:12:04 +0530

    commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SuccessAlertComponent,
+    WarningAlertComponent,
+  
+  ],
+  imports: [
+    BrowserModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+
+
+
